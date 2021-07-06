9b0b56892cb74a0b90739a37a1e4741133de406b
Rodolfo S <rnsm14@gmail.com>
2021-07-06 19:25:27 +0200

    Register page done, added some functions.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return ( 
-        <h1>Hi from Register</h1>
-     );
-}
- 
+  //State for Login
+
+  const [user, saveUser] = useState({
+    name: '',
+    email: "",
+    password: "",
+    confirm:''
+  });
+
+  //Exatract from user
+  const { name, email, password, confirm } = user;
+
+  const onChange = (e) => {
+    saveUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //When user wants to login
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    //Validate empty inputs
+
+    //Pass to action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Register</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your name"
+              value={name}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="confirm">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm"
+              name="confirm"
+              placeholder="Repeat your Password"
+              value={confirm}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Register"
+            />
+          </div>
+        </form>
+        <Link to={"/"} className="enlace-cuenta">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default Register;
