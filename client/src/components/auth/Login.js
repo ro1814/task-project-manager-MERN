fcecd5b4e44503103c00f97f2d3c5eeda4723f16
Rodolfo S <rnsm14@gmail.com>
2021-07-06 18:54:30 +0200

    some styles, forms, divs added to the Login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,41 @@
 import React from 'react'
 
 const Login = () => {
-    return (  <h1> Hi from Login</h1> );
+
+    const onChange = () => {
+
+    }
+
+    return (  <div className="form-usuario">
+        <div className="contenedor-form sombra-dark">
+            <h1>Login</h1>
+            <form>
+                <div className='campo-form'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email'
+                    id='email'
+                    name='email'
+                    placeholder='Email'
+                    onChange={onChange}/>
+                </div>
+
+                <div className='campo-form'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password'
+                    id='password'
+                    name='password'
+                    placeholder='Password'
+                    onChange={onChange}/>
+                </div>
+
+                <div className='campo-form'>
+                    <input type="submit"
+                    className='btn btn-primario btn-block'
+                    value='Login'/>
+                </div>
+            </form>
+        </div>
+    </div> );
 }
  
 export default Login;
