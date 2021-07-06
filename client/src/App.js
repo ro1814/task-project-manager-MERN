65fe3c54d50f9850e547c54381b24731c48f2b7c
Rodolfo S <rnsm14@gmail.com>
2021-07-06 18:24:51 +0200

    Some misspelling words fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>MERN task</h1>
+    <Router>
+
+      <Switch>
+      <Route exact path='/' component = {Login} />
+      <Route exact path='/register' component = {Register} />
+      <Route exact path='/projects' component = {Projects} />
+
+      </Switch>
+
+    </Router>
   );
 }
 
